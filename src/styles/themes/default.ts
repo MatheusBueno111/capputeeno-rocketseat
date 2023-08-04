63bfa04193101ad8db93fe5d641cd5e2ebcec857
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2023-08-04 19:13:52 -0300

    add: config themes

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,1 +1,18 @@
-export const defaultTheme = {} as const
+export const defaultTheme = {
+  colors: {
+    'text-dark': '#41414D',
+    'text-span': '#617480',
+    text: '#737380',
+    divider: '#DCE2E5',
+    background: '#F0F0F5',
+    red: '#DE3838',
+    blue: '#115D8C',
+    green: '#51B853',
+    orange: '#FFA585',
+    white: '#FFFFFF',
+    price: '#09090A',
+  },
+  fonts: {
+    text: 'Sairo',
+  },
+} as const
